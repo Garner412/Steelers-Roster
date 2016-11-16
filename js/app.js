fd35f46f8640b4489bbcc0fe33c0303c97f93e05
Greg Garner <gpgarner10@gmail.com>
2016-11-16 13:02:20 -0600

    fix styling size

--- js/app.js
+++ js/app.js
@@ -1,15 +1,2 @@
 var app = angular.module('SteelersApp', [
 ])
-// .config(function ( $routeProvider ) {
-
-//   $routeProvider
-//     .when('/team', {
-//       templateUrl: 'views/team.html',
-//       controller: 'TeamCtrl',
-//       controllerAs: 'team'
-//     })
-//     .otherwise({
-//       redirectTo: '/team'
-//     });
-
-// });
